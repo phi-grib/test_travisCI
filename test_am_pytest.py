a873469c907c53ac1b9d2bc4e1d1949bf7553f56
BielStela <biel.stela@gmail.com>
2018-06-07 10:22:46 +0200

    test broken test

--- test_am_pytest.py
+++ test_am_pytest.py
@@ -1,9 +1,11 @@
 from astonishing_math import multiply, suma
 
+
 def test_numbers_3_4():
-    assert multiply(3,4) == 12
+    assert multiply(3, 4) == 12
     assert suma(3, 4) == 7
- 
+
+
 def test_strings_a_3():
-    assert multiply('a',3) == 'aaa' 
-    assert suma('a','a') == 'aa'
+    assert multiply('a', 3) == 'aa'
+    assert suma('a', 'a') == 'aa'
